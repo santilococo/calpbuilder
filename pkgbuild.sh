2b2fd205289c7338cc8b8c9cd48ec01e382b52e2
Santiago Lo Coco <santilococo.01@gmail.com>
2022-02-27 20:34:36 -0300

    Add calbuilder user

--- pkgbuild.sh
+++ pkgbuild.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
 pacman -Syu --noconfirm --needed base-devel
+
+useradd calbuilder -m
+echo "calbuilder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+visudo -c
 chmod -R a+rw .
 
 baseDir="$PWD"
 cd "${INPUT_PKGDIR:-.}"
 oldFiles=$(find -H "$PWD")
 
-sudo -H -u nobody makepkg --syncdeps --noconfirm
+sudo -H -u calbuilder makepkg --syncdeps --noconfirm
 
-sudo -u nobody makepkg --printsrcinfo > .SRCINFO
+sudo -u calbuilder makepkg --printsrcinfo > .SRCINFO
 echo "::set-output name=srcInfo::.SRCINFO"
 sudo mv .SRCINFO /github/workspace
 
-pkgFile=$(sudo -u nobody makepkg --packagelist)
+pkgFile=$(sudo -u calbuilder makepkg --packagelist)
 relPkgFile="$(realpath --relative-base="$baseDir" "$pkgFile")"
 echo "::set-output name=pkgFile::$relPkgFile"
 sudo mv "$pkgFile" /github/workspace
