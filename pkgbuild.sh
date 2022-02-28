9923bfc64108ebfd2af83ea22bd8f20dcf91ab00
Santiago Lo Coco <santilococo.01@gmail.com>
2022-02-28 20:08:23 -0300

    Add runScript()

--- pkgbuild.sh
+++ pkgbuild.sh
@@ -1,29 +1,33 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+runScript() {
+    set -euo pipefail
 
-pacman -Syu --noconfirm base-devel
+    pacman -Syu --noconfirm base-devel
 
-echo "nobody ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-visudo -c
-chmod -R a+rw .
+    echo "nobody ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+    visudo -c
+    chmod -R a+rw .
 
-baseDir="$PWD"
-[ -n "$INPUT_PKGDIR" ] && inBaseDir=true || inBaseDir=false
-cd "${INPUT_PKGDIR:-.}"
-oldFiles=$(find -H "$PWD" -not -path '*.git*')
+    baseDir="$PWD"
+    [ -n "$INPUT_PKGDIR" ] && inBaseDir=true || inBaseDir=false
+    cd "${INPUT_PKGDIR:-.}"
+    oldFiles=$(find -H "$PWD" -not -path '*.git*')
 
-sudo -u nobody makepkg -s --noconfirm
+    sudo -u nobody makepkg -s --noconfirm
 
-sudo -u nobody makepkg --printsrcinfo > .SRCINFO
-echo "::set-output name=srcInfo::.SRCINFO"
-[ $inBaseDir = false ] && mv .SRCINFO /github/workspace
+    sudo -u nobody makepkg --printsrcinfo > .SRCINFO
+    echo "::set-output name=srcInfo::.SRCINFO"
+    [ $inBaseDir = false ] && mv .SRCINFO /github/workspace
 
-pkgFile=$(sudo -u nobody makepkg --packagelist)
-relPkgFile="$(realpath --relative-base="$baseDir" "$pkgFile")"
-echo "::set-output name=pkgFile::$relPkgFile"
-[ $inBaseDir = false ] && mv "$pkgFile" /github/workspace
+    pkgFile=$(sudo -u nobody makepkg --packagelist)
+    relPkgFile="$(realpath --relative-base="$baseDir" "$pkgFile")"
+    echo "::set-output name=pkgFile::$relPkgFile"
+    [ $inBaseDir = false ] && mv "$pkgFile" /github/workspace
 
-newFiles=$(find -H "$PWD" -not -path '*.git*')
-mapfile -t toRemove < <(printf '%s\n%s\n' "$newFiles" "$oldFiles" | sort | uniq -u)
-rm -rf "${toRemove[@]}"
+    newFiles=$(find -H "$PWD" -not -path '*.git*')
+    mapfile -t toRemove < <(printf '%s\n%s\n' "$newFiles" "$oldFiles" | sort | uniq -u)
+    rm -rf "${toRemove[@]}"
+}
+
+runScript "$@"
