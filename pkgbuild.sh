7db52b1cbf16702a4860180d19f082aa20d12653
Santiago Lo Coco <santilococo.01@gmail.com>
2022-02-28 19:59:52 -0300

    Fix toRemove bug

--- pkgbuild.sh
+++ pkgbuild.sh
@@ -11,7 +11,7 @@ chmod -R a+rw .
 baseDir="$PWD"
 [ -n "$INPUT_PKGDIR" ] && inBaseDir=true || inBaseDir=false
 cd "${INPUT_PKGDIR:-.}"
-oldFiles=$(find -H "$PWD")
+oldFiles=$(find -H "$PWD" -not -path '*.git*')
 
 sudo -u nobody makepkg -s --noconfirm
 
@@ -24,6 +24,6 @@ relPkgFile="$(realpath --relative-base="$baseDir" "$pkgFile")"
 echo "::set-output name=pkgFile::$relPkgFile"
 [ $inBaseDir = false ] && mv "$pkgFile" /github/workspace
 
-newFiles=$(find -H "$PWD")
-toRemove=$(printf '%s\n%s\n' "$newFiles" "$oldFiles" | sort | uniq -u)
-rm -rf "$toRemove"
+newFiles=$(find -H "$PWD" -not -path '*.git*')
+mapfile -t toRemove < <(printf '%s\n%s\n' "$newFiles" "$oldFiles" | sort | uniq -u)
+rm -rf "${toRemove[@]}"
