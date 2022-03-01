0bfe96e621963f6c19350d27d2c1b58f52f08464
Santiago Lo Coco <santilococo.01@gmail.com>
2022-03-01 13:13:05 -0300

    Build with calbuilder user

--- pkgbuild.sh
+++ pkgbuild.sh
@@ -35,7 +35,7 @@ installAurDeps() {
 importPrivateKey() {
     echo "$gpgPrivateKey" > private.key
     gpgFlags=("--batch" "--pinentry-mode" "loopback" "--passphrase")
-    sudo -H -u nobody gpg "${gpgFlags[@]}" "$gpgPassphrase" --import private.key
+    sudo -Hu calbuilder gpg "${gpgFlags[@]}" "$gpgPassphrase" --import private.key
     rm private.key
     sedCommand="gpg ${gpgFlags[*]} \"$gpgPassphrase\""
     makepkgSigFile="/usr/share/makepkg/integrity/generate_signature.sh"
@@ -45,9 +45,9 @@ importPrivateKey() {
 buildPackage() {
     if [ -n "$gpgPrivateKey" ] && [ -n "$gpgPublicKey" ]; then
         importPrivateKey
-        sudo -H -u nobody makepkg -s --sign --key "$gpgPublicKey" --noconfirm
+        sudo -Hu calbuilder makepkg -s --sign --key "$gpgPublicKey" --noconfirm
     else
-        sudo -u nobody makepkg -s --noconfirm
+        sudo -Hu calbuilder makepkg -s --noconfirm
     fi
 }
 
