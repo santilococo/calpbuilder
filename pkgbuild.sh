fa1adfe4cf2f65098709f31456ad6439c72796c3
Santiago Lo Coco <santilococo.01@gmail.com>
2024-02-09 23:43:47 -0300

    Fix typos

--- pkgbuild.sh
+++ pkgbuild.sh
@@ -44,7 +44,7 @@ importPrivateKey() {
     gpgFlags=("--batch" "--pinentry-mode" "loopback" "--passphrase")
     sudo -Hu calbuilder gpg "${gpgFlags[@]}" "$gpgPassphrase" --import private.key
     rm private.key
-    gpgPasshraseEscaped="$(echo "$gpgPassphrase" | sed 's/\([&|\]\)/\\\1/g')"
+    gpgPassphraseEscaped="$(echo "$gpgPassphrase" | sed 's/\([&|\]\)/\\\1/g')"
     sedCommand="gpg ${gpgFlags[*]} \'$gpgPassphraseEscaped\'"
     makepkgSigFile="/usr/share/makepkg/integrity/generate_signature.sh"
     sed -i -e "s/gpg/$sedCommand/" $makepkgSigFile
@@ -54,7 +54,6 @@ buildPackage() {
     pacman -S --noconfirm pacman-contrib
     sudo -Hu calbuilder updpkgsums
     if [ -n "$gpgPrivateKey" ] && [ -n "$gpgPublicKey" ]; then
-	printWarnings "Inside GPG"
         importPrivateKey
         sudo -Hu calbuilder makepkg -s --sign --key "$gpgPublicKey" --noconfirm
     else
